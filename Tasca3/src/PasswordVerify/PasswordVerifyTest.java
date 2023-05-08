3c2d7ba8ce08b2510419c61dea00572393b3c819
Jean M <115979940+Alum1Dam@users.noreply.github.com>
2023-05-08 22:32:34 +0200

    Cuarto Test

--- Tasca3/src/PasswordVerify/PasswordVerifyTest.java
+++ Tasca3/src/PasswordVerify/PasswordVerifyTest.java
@@ -28,9 +28,20 @@ class PasswordVerifyTest {
 
     @Test
     void validaPassword3() {
-        PasswordResult esperado = new PasswordResult(false, "La contrasenya ha de contenir almenys 2 números\n" + "La contrasenya ha de contenir almenys una lletra majúscula\n"
+        PasswordResult esperado = new PasswordResult(false, "La contrasenya ha de contenir almenys 2 números\n"
+                + "La contrasenya ha de contenir almenys una lletra majúscula\n"
                 + "La contrasenya ha de contenir almenys un caràcter especial");
         PasswordResult actual = PasswordVerify.validaPassword("holacaracola");
+        assertEquals(esperado.isValid(), actual.isValid());
+        assertEquals(esperado.getMsjError(), actual.getMsjError());
+    }
+
+    @Test
+    void validaPassword4() {
+        PasswordResult esperado = new PasswordResult(false, "La contrasenya ha de tenir almenys 8 caràcters\n"
+                + "La contrasenya ha de contenir almenys una lletra majúscula\n"
+                + "La contrasenya ha de contenir almenys un caràcter especial");
+        PasswordResult actual = PasswordVerify.validaPassword("hola12");
         assertEquals(esperado.isValid(), actual.isValid());
         assertEquals(esperado.getMsjError(), actual.getMsjError());
     }
